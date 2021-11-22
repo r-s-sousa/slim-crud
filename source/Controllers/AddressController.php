099bca47ab1c2b70b9ab68967262215ae4357ac0
Rafael <rafael_sousa2018@outlook.com>
2021-11-21 21:33:30 -0300

    API update returns responses

--- source/Controllers/AddressController.php
+++ source/Controllers/AddressController.php
@@ -2,9 +2,8 @@
 
 namespace Source\Controllers;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Request;
+use Slim\Http\Response;
 use Source\Models\Endereco;
 
 class AddressController
@@ -16,46 +15,36 @@ class AddressController
         $this->obEndereco = new Endereco();
     }
 
-    public function getAllAddress(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-        $dadosJson = json_encode($this->obEndereco->getAllAndresses());
-        $response->getBody()->write($dadosJson);
-        return $response;
+    public function getAllAddress(Request $request, Response $response, array $args): Response {
+        return $response->withJson($this->obEndereco->getAllAndresses(), 200);
     }
 
-    public function getAddressById(Request $request, ResponseInterface $response, array $args): ResponseInterface
+    public function getAddressById(Request $request, Response $response, array $args): Response
     {
         $id = $args['id'];
-        $dadosJson = json_encode($this->obEndereco->getAddressById($id) ?? ['error' => "404 not found"]);
-        $response->getBody()->write($dadosJson);
-
-        return $response;
+        $dados = $this->obEndereco->getAddressById($id) ?? ['error' => "404 not found"];
+        return $response->withJson($dados, 200);
     }
 
-    public function createAddress(Request $request, ResponseInterface $response, array $args): ResponseInterface
+    public function createAddress(Request $request, Response $response, array $args): Response
     {
         $endereco = $request->getParsedBody()['endereco'];
-        $obEndereco = json_encode($this->obEndereco->save($endereco));
-        $response->getBody()->write($obEndereco);
-
-        return $response;
+        $obEndereco = $this->obEndereco->save($endereco);
+        return $response->withJson($obEndereco, 201);
     }
 
-    public function updateAddressById(Request $request, ResponseInterface $response, array $args): ResponseInterface
+    public function updateAddressById(Request $request, Response $response, array $args): Response
     {
         $id = $args['id'];
         $endereco = $request->getParsedBody()['endereco'];
-        $obEndereco = json_encode($this->obEndereco->update($id, $endereco) ?? ['error' => "Não foi possível atualizar endereço"]);
-        $response->getBody()->write($obEndereco);
-
-        return $response;
+        $obEndereco = $this->obEndereco->update($id, $endereco) ?? ['error' => "Não foi possível atualizar endereço"];
+        return $response->withJson($obEndereco, 200);
     }
 
-    public function deleteAddressById(Request $request, ResponseInterface $response, array $args): ResponseInterface
+    public function deleteAddressById(Request $request, Response $response, array $args): Response
     {
         $id = $args['id'];
-        $obEndereco = json_encode($this->obEndereco->delete($id) ?? ['error' => "Não foi possível deletar endereço"]);
-        $response->getBody()->write($obEndereco);
-
-        return $response;
+        $obEndereco = $this->obEndereco->delete($id) ?? ['error' => "Não foi possível deletar endereço"];
+        return $response->withJson($obEndereco, 200);
     }
 }
